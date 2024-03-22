382f682d8a6737a53cb225754097a1f90bd44a8b
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-03-22 16:36:26 +0530

    Learnt about Datatype

---
+++ DataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3)
+console.log("Ash")
+
+//Number= 2 to the power 54
+//bigint
+//string =>""
+//boolean => True/False
+//null => standalone Value
+//undefined => 
+// symbol => unique
+//object 
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+ 
