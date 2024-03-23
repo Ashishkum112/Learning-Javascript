c518bf653e9743dcdba984bd7e128a0e66ad3668
Ashishkum112 <ashishkumarr0214@gmail.com>
2024-03-24 01:14:27 +0530

    data type summary

--- Datatype_Summary.js
+++ Datatype_Summary.js
@@ -3,6 +3,47 @@
 // 7 types : String,Number,Boolean,null,
 // ,undefined,Symbol(unique),BigInt(Big Values)
 
-// Reference (Non primitive)
+/*
 
-// Array, Objects, Functions
+
+// Number ;
+const score =33 
+const scoreValue = 133.2 
+
+const isLogged = false
+const out= null
+let userEmail = undefined // Both returns undefined
+let userEmail ; // Returns undefined
+
+
+
+
+const id = Symbol('123')
+const another_id = Symbol('123')
+
+console.log(id == another_id)
+
+*/
+
+// Reference (Non primitive)
+ 
+// Array, 
+
+const Heroes = ["Aquaman","Batman","Superman"]; // Array 
+
+//  Objects, 
+
+let details= {
+    name : " Ashish",
+    age : 24,
+    class : "Sec A",
+    branch : "Ece"
+}
+
+// Functions
+
+const myFunction= function(){
+    console.log("Hello World");
+}
+
+console.log(typeof details)
